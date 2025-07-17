9451da054ebded4171a586f69c50154ce0a4f415
644731871zzz <644731871zzz@gmail.com>
2025-07-17 18:54:58 +0800

    20250717

--- book1/36/exersice_02.py
+++ book1/36/exersice_02.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 import streamlit as st
+import seaborn as sns
 
 #配置默认视图样式
 #rc的意思是run commands表示配置脚本的初始化命令集初始文件,后面的params表示配置文件中的参数
@@ -35,20 +36,56 @@ with st.sidebar:
     
 #质心
 mu = [mu_X1,mu_X2]
+#协方差矩阵
+Sigma = [[sigma_X1**2,sigma_X1*sigma_X2*rho],
+         [sigma_X1*sigma_X2*rho,sigma_X2**2]]
+
+width = 4
+x1 = np.linspace(-width,width,321)
+x2 = np.linspace(-width,width,321)
+xx1,xx2 = np.meshgrid(x1,x2)
+xx12 = np.dstack((xx1,xx2))
+bi_norm = multivariate_normal(mu,Sigma)
+#二元高斯PDF
+PDF_joint = bi_norm.pdf(xx12)
+
+
+#生成协方差矩阵具体值
+sigma_x1=1.00;sigma_x2 = 1.3;rho_x12 = -0.5
+mean = [0.6,-0.6] #就是mu
+cov = [[sigma_x1**2,sigma_x1*sigma_x2*rho_x12],
+         [sigma_x1*sigma_x2*rho_x12,sigma_x2**2]] #是sigma
+#生成的点是两列,一列x一列y
+normal_joint=np.random.multivariate_normal(mean, cov,1000)
+
+
+#绘制二院高斯PDF等高线
+fig,ax= plt.subplots(figsize=(5,5))
+
+plt.contourf(xx1,xx2,PDF_joint,20,cmap = 'RdYlBu_r') #20表示登高区域数量
+plt.axvline(x = mu_X1,color = 'k',linestyle = '--')
+plt.axhline(y = mu_X2,color = 'k',linestyle = '--')
+
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+
+st.pyplot(fig)
+
+#使用seaborn绘制具体值的图像
+#jointplot不允许位置参数,必须关键字参数
+g=sns.jointplot(x=normal_joint[:,0],y=normal_joint[:,1],
+                   kind='scatter',marginal_kws={'bins':20},
+                   height=5)
+st.pyplot(g.fig)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+g = sns.jointplot(x=normal_joint[:,0],
+                  y=normal_joint[:,1],
+                  kind='scatter',
+                  marginal_kws={'bins':20},
+                  height=5)
+st.pyplot(g.fig)
 
 
 
---
+++ book1/36/exersice_03.py
@@ -0,0 +1,57 @@
+#精细化构建交互式可视化
+import plotly.graph_objects as go
+import numpy as np
+import streamlit as st
+#多元正态分布对象
+from scipy.stats import multivariate_normal
+
+st.latex(r'''{\displaystyle f_{\mathbf {X} }(x_{1},\ldots,x_{k})=
+         {\frac {\exp \left(-{\frac {1}{2}}
+        ({\mathbf {x} }-{\boldsymbol {\mu }})
+        ^{\mathrm {T} }{\boldsymbol {\Sigma }}^{-1}
+        ({\mathbf {x} }-{\boldsymbol {\mu }})\right)}
+          {\sqrt {(2\pi )^{k}|{\boldsymbol {\Sigma}}|}}}}''')
+
+def bmatrix(a):
+    """返回latex形式的bmatrix
+    a是numpy array
+    返回值为bmatrix字符串
+    """
+    #验证维度,bmatrix只能打印2d矩阵
+    if len(a.shape) > 2:
+        raise ValueError('bmatrix can at most display two dimensions')
+    #字符串化后,将字符串中的[]删除,仅剩下纯数字和空白
+    lines = str(a).replace('[','').replace(']','').splitlines()
+    #latex矩阵环境的起始命令
+    rv = [r'\begin{bmatrix}']
+    #split表示按照空白符号(空格,制表符等)拆成若干元素
+    #用&字符分开的元素联结起来
+    #'  '表示在每个首行添加两个空格,r'\\'表示换行命令
+    rv += ['  ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    #矩阵环境结束命令
+    rv += [r'\end{bmatrix}']
+    #用换行符将目标连接成多行文本 在他们之间插入\n换行符
+    return '\n'.join(rv)
+
+
+xxx1,xxx2,xxx3 = np.mgrid[-3:3:0.2,-3:3:0.2,-3:3:0.2]
+
+with st.sidebar:
+    st.title('Triariate Gaussian Distribution')
+    sigma_1 = st.slider('sigma_1',min_value = 0.5,max_value = 3.0,value = 1.0,step = 0.1)
+    sigma_2 = st.slider('sigma_2',min_value = 0.5,max_value = 3.0,value = 1.0,step = 0.1)
+    sigma_3 = st.slider('sigma_3',min_value = 0.5,max_value = 3.0,value = 1.0,step = 0.1)
+    
+    rho_1_2 = st.slider('rho_1_2',min_value = -0.95,maxvalue = 0.95,value = 0.0,step = 0.05)
+    rho_1_3 = st.slider('rho_1_3',min_value = -0.95,maxvalue = 0.95)
+
+         
+         
+
+
+
+
+
+
+
+
