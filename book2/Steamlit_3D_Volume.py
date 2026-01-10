7e8da2f1eb6ffe66f7a08d1515ac11c50102aa01
644731871zzz <644731871zzz@gmail.com>
2026-01-10 22:03:50 +0800

    20260110

--- book2/Steamlit_3D_Volume.py
+++ book2/Steamlit_3D_Volume.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import cm
 import plotly.graph_objects as go
 import streamlit as st
 
@@ -17,28 +19,20 @@ def bmatrix(a):
     if len(a.shape) > 2:
         raise ValueError('bmatrix can at most display two dimensions')
         
-    #replace是转换第一个参数成第二个参数.这里相当于删除 最后一个方法表示按照行切分,将每一行切成一个一个的字符串
     lines = str(a).replace('[','').replace(']','').splitlines()
-    #rv表示返回值的命名
-    #r表示python不再读取字符串中的\,但是这里LaTeX引擎还是会读取\
-        #python中只有字符串内才解析'\',使用r将所有python关于\的转义解析,其他功能调用的\正常
-            #转移解析属于字符串下
-        #latex来自streamlit,因为需要渲染
     rv = [r'\begin{bmatrix}']
-    rv += ['   ' + ' & '.join(l.split()) + r'\\' for l in lines]#
-    rv += [r'\end{bmatrix}']#
+    rv += ['   ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    rv += [r'\end{bmatrix}']
     return '\n'.join(rv)
 
 
 with st.sidebar:
-    st.title('三维等值面')
-    #这里返回的字符串是包括\的,python没有执行
-    #这个字符串bmatrix不是前端调用python,而是用python生成字符串,作用是前端的函数语法
+    st.title('体积图')
     st.latex(r'''Q = \begin{bmatrix}
              a & b & c\\
                  b & d & e\\
                      c & e & f
-                     \end{bmatrix}''')#仅仅是字符串的显示,是字母表示的矩阵形状,没有数字
+                     \end{bmatrix}''')
                      
     a = st.slider('a', -5,5,1,1)
     b = st.slider('b', -5,5,0,1)
@@ -60,17 +54,17 @@ Q = np.array([[a,b,c],
               [b,d,e],
               [c,e,f]])
 
-st.latex('Q = ' + bmatrix(Q))#这里显示了实际的矩阵,有数字
-fff = np.diag(Points @ Q @Points.T)#对角化,只取对角线
+st.latex('Q = ' + bmatrix(Q))
+fff = np.diag(Points @ Q @Points.T)
 
-fig = go.Figure(data = go.Isosurface(
+fig = go.Figure(data = go.Volume(
     x = X.flatten(),
     y = Y.flatten(),
     z = Z.flatten(),
     value = fff.flatten(),
-    surface_count = 8,
+    opacity = 0.18,
+    surface_count = 11,
     colorscale = 'RdYlBu',
-    caps = dict(x_show = False,y_show = False)
 ))
 
 fig.update_layout(autosize = False,
