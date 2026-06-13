ad6f060c2a7e6a0900960162bc1d2cc0500f60cf
644731871zzz <644731871zzz@gmail.com>
2026-06-13 21:58:16 +0800

    20260613

--- book4/streamlit0401.py
+++ book4/streamlit0401.py
@@ -60,7 +60,7 @@ A = np.array([[a,b],
               [c,d]],dtype = float)
 X = np.array(xv)
 Y = np.array(yv)
-Txvyv = A@np.stack((X,Y))#
+Txvyv = A@np.stack((X,Y))#stack默认是按照第0维度堆叠
 
 X = np.array(xh)
 Y = np.array(yh)
@@ -70,15 +70,34 @@ st.latex(r'A = ' + bmatrix(A))
 a1 = A[:,0].reshape((-1,1))
 a2 = A[:,1].reshape((-1,1))
 
+#latex语法里\n换行会被当成空格 \\才是现实上的换行
 st.latex(r'''
-         a_1 = Ae_1 = ''' + bmatrix(A) + 'e_1 = ' + bmatrix(a1)) #
+         a_1 = Ae_1 = ''' + bmatrix(A) + 'e_1 = ' + bmatrix(a1)) 
 st.latex(r'''
-         a_2 = Ae_2 = ''' + bmatrix(A) + 'e_2 = ' + bmatrix(a2)) #
+         a_2 = Ae_2 = ''' + bmatrix(A) + 'e_2 = ' + bmatrix(a2))
 st.latex(r'\begin{vmatrix} A \end{vmatrix} = ' + str(np.linalg.det(A)))
 
 square_x = np.array([0,1,1,0])
 square_y = np.array([0,0,1,1])
 square_array = np.stack((square_x,square_y))
+
+fig.add_trace(go.Scatter(x = square_x,y = square_y,fill = 'toself',line_color = 'orange'),1,1)
+A_times_square_array = A @ square_array
+
+fig.add_trace(go.Scatter(x = A_times_square_array[0,:],y = A_times_square_array[1,:],
+                         fill = 'toself',line_color = 'orange'),1,2)
+fig.add_trace(go.Scatter(x = Txvyv[0],y = Txvyv[1],mode = 'lines',line_width = lw,line_color = 'red'),1,2)
+fig.add_trace(go.Scatter(x = Txhyh[0],y = Txhyh[1],mode = 'lines',line_width = lw,line_color = 'blue'),1,2)
+
+fig.update_xaxes(range = [-4,4])
+fig.update_yaxes(range = [-4,4])
+fig.update_layout(width = 800,height = 500,showlegend = False,template = 'none', #不显示图例,不使用模板
+                  #背景颜色设为白色,不显示x嗯y的网格线
+                  plot_bgcolor = 'white',yaxis2_showgrid = False,xaxis2_showgrid = False)
+
+st.plotly_chart(fig)
+
+
 
 
 
